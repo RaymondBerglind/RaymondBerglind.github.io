2d1ec0c76207c181beb9241ceae152e35f04f6b9
RaymondBerglind <raymond.berglind@gmail.com>
2016-07-16 19:25:53 +0200

    Fixed bug that caused menu to disappear on large device when option was chosen.

--- responsive.js
+++ responsive.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
     $('.icon, ul.nav li').click(function() {
-        var isVisible = $('ul.nav li').is(':visible');
-        if (!isVisible) {
-            $('ul.nav').toggleClass("responsive");
-            $('ul.nav li').slideDown(200);
-        } else {
-            $('ul.nav li').slideUp(200, function() {
+        var windowWidth = $(window).width();
+        if (windowWidth < 800) {
+            var isVisible = $('ul.nav li').is(':visible');
+            if (!isVisible) {
                 $('ul.nav').toggleClass("responsive");
-            });
+                $('ul.nav li').slideDown(200);
+            } else {
+                $('ul.nav li').slideUp(200, function() {
+                    $('ul.nav').toggleClass("responsive");
+                });
+            }
         }
     });
 });
