f893eece5086d74d3c11d33e511f534fd35ba40e
RaymondBerglind <raymond.berglind@gmail.com>
2016-07-29 18:54:06 +0200

    Fixed yet another menu problem where menu state for CSS would get out of sync.

--- responsive.js
+++ responsive.js
@@ -4,19 +4,27 @@ $(document).ready(function() {
     function getWindowWidth() {
         return $(window).width();
     }
+
+    function menuIsVisible() {
+        return $('ul.nav li').is(':visible');
+    }
+
+    function menuIsInline() {
+        return $('ul.nav li').css('display') === 'inline';
+    }
+
     function widthCheck() {
         if (getWindowWidth() > 800) {
             $('ul.nav').removeClass(responsiveClass);
             $('ul.nav li').css('display', 'inline');
-        } else {
+        } else if (!menuIsVisible() || menuIsInline()){
             $('ul.nav li').css('display', 'none');
         }
     }
 
     $('.icon, ul.nav li').click(function() {
         if (getWindowWidth() <= 800) {
-            var isVisible = $('ul.nav li').is(':visible');
-            if (!isVisible) {
+            if (!menuIsVisible()) {
                 $('ul.nav').toggleClass(responsiveClass);
                 $('ul.nav li').slideDown(200);
             } else {
