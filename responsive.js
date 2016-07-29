acdedca04415372fda6a0b68eebfb620d8e67dbb
RaymondBerglind <raymond.berglind@gmail.com>
2016-07-29 18:39:01 +0200

    Fixed menu problem where list items would disappear on screen resize after having been expanded.

--- responsive.js
+++ responsive.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
+    var responsiveClass = 'responsive';
+
+    function getWindowWidth() {
+        return $(window).width();
+    }
+    function widthCheck() {
+        if (getWindowWidth() > 800) {
+            $('ul.nav').removeClass(responsiveClass);
+            $('ul.nav li').css('display', 'inline');
+        } else {
+            $('ul.nav li').css('display', 'none');
+        }
+    }
+
     $('.icon, ul.nav li').click(function() {
-        var windowWidth = $(window).width();
-        if (windowWidth < 800) {
+        if (getWindowWidth() <= 800) {
             var isVisible = $('ul.nav li').is(':visible');
             if (!isVisible) {
-                $('ul.nav').toggleClass("responsive");
+                $('ul.nav').toggleClass(responsiveClass);
                 $('ul.nav li').slideDown(200);
             } else {
                 $('ul.nav li').slideUp(200, function() {
-                    $('ul.nav').toggleClass("responsive");
+                    $('ul.nav').toggleClass(responsiveClass);
                 });
             }
         }
     });
+
+    widthCheck();
+    $(window).resize(widthCheck);
 });
